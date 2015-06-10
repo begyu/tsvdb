80e55c349fd5a720654c3b5289d6414eb49b5657
begyu <begyu@uw.hu>
2015-06-10 11:40:10 +0200

    0.9.24
    fix strtrimcmp

--- tsvdb.c
+++ tsvdb.c
@@ -1178,7 +1178,7 @@ int strtrimcmp(const char *s1, const char *s2)
 {
     while (*s1++ == *s2++)
     {
-        if ((*s1 == ' ') || (*s1 == '\0'))
+        if ((*s1 == '\0') || (*s1 == ' ' && s1[1] < '!'))
              	return 0;
     }
     return -1;
